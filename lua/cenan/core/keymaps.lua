a8a4dafbccfff06510d5f3c7b8b73e43a88a0796
Cenan OZEN <cenan.ozen@gmail.com>
2025-01-14 19:41:18 +0300

    Add close buffer shortcut

--- lua/cenan/core/keymaps.lua
+++ lua/cenan/core/keymaps.lua
@@ -6,5 +6,19 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "//", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<Tab>", ":bn<CR>", { desc = "Move to next buffer" })
 keymap.set("n", "<S-Tab>", ":bp<CR>", { desc = "Move to previous buffer" })
+keymap.set('n', '<leader>x', function()
+    -- Check if the buffer has unsaved changes
+    if vim.bo.modified then
+        local choice = vim.fn.confirm("Buffer has unsaved changes. Save?", "&Yes\n&No\n&Cancel")
+        if choice == 1 then
+            vim.cmd('write')
+            vim.cmd('bdelete')
+        elseif choice == 2 then
+            vim.cmd('bdelete!')
+        end
+    else
+        vim.cmd('bdelete')
+    end
+end, { noremap = true, silent = true, desc = "Close buffer" })
 keymap.set('n', '<leader>lf', vim.lsp.buf.format, { noremap = true, silent = true, desc = 'Format buffer' })
 
