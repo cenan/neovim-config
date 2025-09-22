184ddce3dff9a1244cec8054e3effe73ba1c10b6
cenan <cenan.ozen@gmail.com>
2025-09-22 10:31:29 +0300

    Remove ts_ls from requied lsp server list

--- lua/cenan/plugins/lsp/mason.lua
+++ lua/cenan/plugins/lsp/mason.lua
@@ -24,7 +24,6 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "ts_ls",
         "html",
         "cssls",
         "lua_ls",
